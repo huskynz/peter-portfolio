cf6cb6519f71a81bc9a67647180bf2df6f5dea2c
Peter Gallwas <peter@husky.nz>
2023-11-07 13:17:12 +1300

    ah do she wordk

--- components/whereilive.tsx
+++ components/whereilive.tsx
@@ -8,11 +8,9 @@ import Iframe from 'react-iframe'
 
 
 export default function About() {
-  const { ref } = useSectionInView("Where I Live");
 
   return (
     <motion.section
-      ref={ref}
       className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
