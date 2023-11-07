d6c466220a543ba4f74f60d7ef3548d54bb1abbd
Peter Gallwas <peter@husky.nz>
2023-11-07 14:10:20 +1300

    sp per and tech
    d

--- components/persskills.tsx
+++ components/persskills.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { skillsData } from "@/lib/data";
+import { pskillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
 
@@ -21,17 +21,16 @@ const fadeInAnimationVariants = {
 };
 
 export default function Skills() {
-  const { ref } = useSectionInView("Skills");
+
 
   return (
     <section
-      id="skills"
-      ref={ref}
+      id="pskills"
       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
     >
-      <SectionHeading>My Tech Skills</SectionHeading>
+      <SectionHeading>My Personal Skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-        {skillsData.map((skill, index) => (
+        {pskillsData.map((skill, index) => (
           <motion.li
             className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
             key={index}
