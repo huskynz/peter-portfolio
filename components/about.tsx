1cadfc12cfc80db3f1dc2d4374152acf7f3f5e95
Peter Gallwas <peter@husky.nz>
2023-12-07 05:53:41 +1300

    Update things

--- components/about.tsx
+++ components/about.tsx
@@ -18,7 +18,7 @@ export default function About() {
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-     <p>Greetings! My name is Peter. Over the past three years, I’ve been refining my skills in Azure, AWS, and Microsoft 365. During this time, I’ve earned two Microsoft Certifications: the AZ-900 and the MS-900. My learning journey continues, and this year, I’ve ventured into full-stack development and film. My ambition is to become a full-fledged Cloud Engineer and Architect. In my spare time, I take pleasure in creating websites like this one and acquiring the knowledge necessary to earn more Microsoft Certifications. I’m excited about what the future holds!</p>
+     <p>Hi! My name is Peter. For the last 3 years I have been working with Azure, AWS, Intune,Windows 7,8.1,10,11 and Windows Server 2012R2,2016,2019,2022,AD,AAD,Hybrid Enviromet (AAD sync), and Microsoft 365. During this time, I’ve earned two Microsoft Certifications: the AZ-900 and the MS-900. My learning journey continues, and this year, I’ve ventured into full-stack development and film. In my spare time, I create websites like this one and study to earn more Certifications. I’m excited about what the future holds!</p>
     </motion.section>
   );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2020-2023 Peter Gallwas. All rights reserved.
+        &copy; 2020-2023 <a href="https://www.husky.nz">Peter Gallwas.</a> All rights reserved. |  Spectrum, it's Assets, Images, and Code is Copyrighted &copy; <a href="https://liamsherwin.tech">Liam Sherwin</a> (Unless otherwise specified)
       </small>
     </footer>
   );
